2d177175721bce619fa1ce9a460d6214e5ae082a
303-01\user <hwjw666@naver.com>
2023-01-19 21:14:25 +0900

    [황지원] - DB연동

---
+++ src/main/java/com/jiwon/spring/ex/config/DatabaseConfig.java
@@ -0,0 +1,37 @@
+package com.jiwon.spring.ex.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import com.jiwon.spring.ex.SpringExampleApplication;
+
+@MapperScan(basePackages="com.jiwon.spring.ex.*")
+@Configuration
+public class DatabaseConfig {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+	
+	@Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+        sessionFactory.setMapperLocations(res);
+
+        return sessionFactory.getObject();
+    }
+
+
+	
+}
